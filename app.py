ec0a17baae2b46e4a5c895b8e1287341d7226d9d
kratiknayak <kratiknayak.7@gmail.com>
2018-11-29 16:08:25 +0530

    Added memory to webchat

--- app.py
+++ app.py
@@ -42,39 +42,63 @@ def getResponse():
 
 @app.route('/api/v1/getInsuranceData', methods=['POST'])
 def getInsuranceData():
-    # data = {'nlp': {'uuid': 'cf771ba7-bc07-4520-8c1f-86f29c185d19', 'intents': [{'slug': 'insurancedetails', 'confidence': 0.99, 'description': None}], 'entities': {'policynumber': [{'value': '3123123', 'raw': '3123123', 'confidence': 0.87}]}, 'language': 'en', 'processing_language': 'en', 'version': '2.12.0-20a34c24', 'timestamp': '2018-11-29T06:12:23.139483+00:00', 'status': 200, 'source': '3123123', 'act': 'assert', 'type': None, 'sentiment': 'neutral'}, 'action_id': 'ed62dc85-d66f-4121-ac56-4034f95e1729', 'conversation': {'id': 'test-1543471909240', 'conversation_id': 'test-1543471909240', 'warning': 'The conversation_id field will be depreciated on January 1st 2018! Switch to using the id field instead', 'language': 'en', 'memory': {'policyNumber': {'value': '3123123', 'raw': '3123123', 'confidence': 0.87}}, 'skill_stack': ['askforinsurancenumber'], 'skill': 'askforinsurancenumber', 'skill_occurences': 2, 'participant_data': {}}}
-
+    # sampledata = {'nlp': {'uuid': 'cf771ba7-bc07-4520-8c1f-86f29c185d19', 'intents': [{'slug': 'insurancedetails', 'confidence': 0.99, 'description': None}], 'entities': {'policynumber': [{'value': '3123123', 'raw': '3123123', 'confidence': 0.87}]}, 'language': 'en', 'processing_language': 'en', 'version': '2.12.0-20a34c24', 'timestamp': '2018-11-29T06:12:23.139483+00:00', 'status': 200, 'source': '3123123', 'act': 'assert', 'type': None, 'sentiment': 'neutral'}, 'action_id': 'ed62dc85-d66f-4121-ac56-4034f95e1729', 'conversation': {'id': 'test-1543471909240', 'conversation_id': 'test-1543471909240', 'warning': 'The conversation_id field will be depreciated on January 1st 2018! Switch to using the id field instead', 'language': 'en', 'memory': {'policyNumber': {'value': '3123123', 'raw': '3123123', 'confidence': 0.87}}, 'skill_stack': ['askforinsurancenumber'], 'skill': 'askforinsurancenumber', 'skill_occurences': 2, 'participant_data': {}}}
     recast_response = json.loads(request.get_data())
     policy_number = recast_response['nlp']['entities']['policynumber'][0]['value']
     conversation_id = recast_response['conversation']['id']
-    insurance_data = session.query(Insurance).filter_by(policy_number=policy_number).first()
-    response = sendMessageToUser(insurance_data,conversation_id)
-    return response
+    insurance_data = session.query(Insurance).filter_by(policy_number=policy_number)
+
+    if insurance_data.count() == 0:
+        response_message_obj = {
+            "type": "text",
+            "content": "No policy found. Please enter valid policy number"
+        }
+    else:
+        response_message_obj = {
+            "type": "text",
+            "content":"Policy found. What would you like to know"
+        }
+        message_sent_response = requests.post(f'https://api.recast.ai/connect/v1/conversations/{conversation_id}/messages',
+                                          headers={'Authorization': f'Token {RECAST_DEVELOPER_TOKEN}'},
+                                          json={"messages": response_message_obj}
+                                          )
+        data_to_store_in_memory = {
+            "memory" : {
+                "policy_number": insurance_data.policy_number,
+                "account_name": insurance_data.account_name,
+                "premium": insurance_data.premium,
+                "expiration_date": insurance_data.expiration_date,
+            },
+            "merge_memory" : "true"
+        }
+        requests.put(f'https://api.recast.ai/build/v1/users/kratiknayak/bots/insurance/versions/v1/builder/conversation_states/{conversation_id}',
+                                            data={data_to_store_in_memory}
+                                           )
+    # response = sendMessageToUser(insurance_data,conversation_id)
+    print(recast_response)
+    return message_sent_response
 
 
-def sendMessageToUser(data, convId):
-    response_message_obj = [{
-        "type": "text",
-        "content": f"Your policy number is {data.policy_number}"
-    },{
-        "type": "text",
-        "content": f"Your policy name is {data.account_name}"
-    },{
-        "type": "text",
-        "content": f"Your policy premium is {data.premium}"
-    },{
-        "type": "text",
-        "content": f"Your policy Expiration date is {data.expiration_date}"
-    }]
-    print(response_message_obj)
-    message_sent_response = requests.post(f'https://api.recast.ai/connect/v1/conversations/{convId}/messages',
-            headers={'Authorization': f'Token {RECAST_DEVELOPER_TOKEN}'},
-            json={"messages": response_message_obj}
-    )
-    print("------------------")
-    print(message_sent_response)
-
-    return "OK"
+# def sendMessageToUser(data, convId):
+#     response_message_obj = [{
+#         "type": "text",
+#         "content": f"Your policy number is {data.policy_number}"
+#     },{
+#         "type": "text",
+#         "content": f"Your policy name is {data.account_name}"
+#     },{
+#         "type": "text",
+#         "content": f"Your policy premium is {data.premium}"
+#     },{
+#         "type": "text",
+#         "content": f"Your policy Expiration date is {data.expiration_date}"
+#     }]
+#     message_sent_response = requests.post(f'https://api.recast.ai/connect/v1/conversations/{convId}/messages',
+#             headers={'Authorization': f'Token {RECAST_DEVELOPER_TOKEN}'},
+#             json={"messages": response_message_obj}
+#     )
+#
+#     return "OK"
 
 
 if __name__ == '__main__':
