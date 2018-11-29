8671afa3fcba61a3f61da4c79d0a3b19814ffb3e
kaundinya5 <kaundinya5@gmail.com>
2018-11-29 22:16:12 +0530

    changed variable name

--- app.py
+++ app.py
@@ -89,9 +89,9 @@ def getInsuranceData():
     url, data = f'https://api.recast.ai/build/v1/users/kratiknayak/bots/insurance/versions/v1/builder/conversation_states/{conversation_id}', {
         "memory": {}}
     data_bytes = bytes(json.dumps(data), encoding='utf8')
-    request = Request(url, method='PUT', data=data_bytes, headers={'Content-Type': 'application/json',
+    req = Request(url, method='PUT', data=data_bytes, headers={'Content-Type': 'application/json',
                                                                    'Authorization': 'Token 6a41bb1be6895ce456977bda97a17fe9'})
-    with urlopen(request) as response:
+    with urlopen(req) as response:
         print(response.read())
     return "OK"
 
