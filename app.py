fad8266aff5ef2f0e7fef4168c38496f432c235b
kratiknayak <kratiknayak.7@gmail.com>
2018-11-29 11:51:20 +0530

    Using recast AI response to fetch user data

--- app.py
+++ app.py
@@ -42,61 +42,37 @@ def getResponse():
 
 @app.route('/api/v1/getInsuranceData', methods=['POST'])
 def getInsuranceData():
-    # SAMPLE DATA
-    # data = {
-    #     "action_id": "5149f96e-b2c5-4e30-96ec-c587ae978d52",
-    #     "conversation": {
-    #         "id": '1f4b092a-8837-4944-b467-f495216e34d5',
-    #         "language": "en",
-    #         "memory": {},
-    #         "participant_data": {},
-    #         "skill": "foo",
-    #         "skill_occurences": 2,
-    #         "skill_stack": []
-    #     },
-    #     "nlp": {
-    #         "act": "assert",
-    #         "entities": {},
-    #         "intents": [],
-    #         "language": "en",
-    #         "processing_language": "en",
-    #         "sentiment": "neutral",
-    #         "source": "This is a sample message.",
-    #         "status": 200,
-    #         "timestamp": "2018-06-29T22:00:50.591367+00:00",
-    #         "type" : "",
-    #         "uuid": "c1bc4e40-ca14-4ce2-bc18-700433b001d9",
-    #         "version": "2.12.0"
-    #     }
-    # }
-    print(json.loads(request.get_data()))
+    # data = {'nlp': {'uuid': 'cf771ba7-bc07-4520-8c1f-86f29c185d19', 'intents': [{'slug': 'insurancedetails', 'confidence': 0.99, 'description': None}], 'entities': {'policynumber': [{'value': '3123123', 'raw': '3123123', 'confidence': 0.87}]}, 'language': 'en', 'processing_language': 'en', 'version': '2.12.0-20a34c24', 'timestamp': '2018-11-29T06:12:23.139483+00:00', 'status': 200, 'source': '3123123', 'act': 'assert', 'type': None, 'sentiment': 'neutral'}, 'action_id': 'ed62dc85-d66f-4121-ac56-4034f95e1729', 'conversation': {'id': 'test-1543471909240', 'conversation_id': 'test-1543471909240', 'warning': 'The conversation_id field will be depreciated on January 1st 2018! Switch to using the id field instead', 'language': 'en', 'memory': {'policyNumber': {'value': '3123123', 'raw': '3123123', 'confidence': 0.87}}, 'skill_stack': ['askforinsurancenumber'], 'skill': 'askforinsurancenumber', 'skill_occurences': 2, 'participant_data': {}}}
 
-    # conversationId = data["conversation"]["id"]
-    # insuranceData = session.query(Insurance).filter_by(policy_number='EBU097711156').first()
-    # response = sendMessageToUser(insuranceData,conversationId)
-    return "response"
+    recast_response = json.loads(request.get_data())
+    policy_number = recast_response['nlp']['entities']['policynumber'][0]['value']
+    conversation_id = recast_response['conversation']['id']
+    insurance_data = session.query(Insurance).filter_by(policy_number=policy_number).first()
+    response = sendMessageToUser(insurance_data,conversation_id)
+    print(response)
+    return response
 
 
-# def sendMessageToUser(data, convId):
-#     responseMessageObj = [{
-#         "type": "text",
-#         "content": f"Your policy number is {data.policy_number}"
-#     },{
-#         "type": "text",
-#         "content": f"Your policy name is {data.account_name}"
-#     },{
-#         "type": "text",
-#         "content": f"Your policy premium is {data.premium}"
-#     },{
-#         "type": "text",
-#         "content": f"Your policy Expiration date is {data.expiration_date}"
-#     }]
-#
-#     message_sent_response = requests.post(f'https://api.recast.ai/connect/v1/conversations/{convId}/messages',
-#             headers={'Authorization': f'Token {RECAST_DEVELOPER_TOKEN}'},
-#             json={"messages": responseMessageObj}
-#     )
-#     return "OK"
+def sendMessageToUser(data, convId):
+    responseMessageObj = [{
+        "type": "text",
+        "content": f"Your policy number is {data.policy_number}"
+    },{
+        "type": "text",
+        "content": f"Your policy name is {data.account_name}"
+    },{
+        "type": "text",
+        "content": f"Your policy premium is {data.premium}"
+    },{
+        "type": "text",
+        "content": f"Your policy Expiration date is {data.expiration_date}"
+    }]
+
+    message_sent_response = requests.post(f'https://api.recast.ai/connect/v1/conversations/{convId}/messages',
+            headers={'Authorization': f'Token {RECAST_DEVELOPER_TOKEN}'},
+            json={"messages": responseMessageObj}
+    )
+    return "OK"
 
 
 if __name__ == '__main__':
