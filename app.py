c626baa8d34fdaa72dc5801e23a131e96590f7fe
kaundinya5 <kaundinya5@gmail.com>
2018-11-30 15:10:47 +0530

    Sending message after query is performed

--- app.py
+++ app.py
@@ -73,16 +73,14 @@ def getInsuranceData():
                 "expiration_date": insurance_data.expiration_date.strftime('%d-%m-%Y')
             }
         }
-
-    
-    message_sent_response = requests.post(f'https://api.recast.ai/connect/v1/conversations/{conversation_id}/messages',
-                                          headers={'Authorization': f'Token {RECAST_DEVELOPER_TOKEN}'},
-                                          json={"messages": response_message_obj})
-
+        
     time.sleep(20)
     store = requests.put(f'https://api.recast.ai/build/v1/users/kratiknayak/bots/insurance/versions/v1/builder/conversation_states/{conversation_id}',
                                             headers={'Authorization': f'Token {RECAST_DEVELOPER_TOKEN}'},
                                             json= data_to_store_in_memory)
+    message_sent_response = requests.post(f'https://api.recast.ai/connect/v1/conversations/{conversation_id}/messages',
+                                          headers={'Authorization': f'Token {RECAST_DEVELOPER_TOKEN}'},
+                                          json={"messages": response_message_obj})
     print(store.text)
     return "OK"
 
