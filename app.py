643214d213996fa753541f374f84e1b972eaeedc
kaundinya5 <kaundinya5@gmail.com>
2018-12-03 10:55:45 +0530

    Added new route

--- app.py
+++ app.py
@@ -32,18 +32,8 @@ def hello():
     insurances = session.query(Insurance).limit(10).all()
     return render_template('index.html', insurances=insurances)
 
-@app.route('/api/v1/getResponse', methods=['GET'])
-def getResponse():
-    client = recastai.Client(RECAST_REQUEST_TOKEN)
-    response = client.build.dialog({'type': 'text', 'content': request.args.get('message')}, "12345")
-    res = {
-        "conversation_id":response.conversation.id,
-        "messages":response.messages[0].content
-    }
-    return json.dumps(res)
 
-
-@app.route('/api/v1/getInsuranceData', methods=['POST'])
+@app.route('/api/v1/get_insurance_data', methods=['POST'])
 def getInsuranceData():
     sampledata = {'nlp': {'uuid': 'cf771ba7-bc07-4520-8c1f-86f29c185d19', 'intents': [{'slug': 'insurancedetails', 'confidence': 0.99, 'description': None}], 'entities': {'policynumber': [{'value': '3123123', 'raw': '3123123', 'confidence': 0.87}]}, 'language': 'en', 'processing_language': 'en', 'version': '2.12.0-20a34c24', 'timestamp': '2018-11-29T06:12:23.139483+00:00', 'status': 200, 'source': '3123123', 'act': 'assert', 'type': None, 'sentiment': 'neutral'}, 'action_id': 'ed62dc85-d66f-4121-ac56-4034f95e1729', 'conversation': {'id': 'test-1543471909240', 'conversation_id': 'test-1543471909240', 'warning': 'The conversation_id field will be depreciated on January 1st 2018! Switch to using the id field instead', 'language': 'en', 'memory': {'policyNumber': {'value': '3123123', 'raw': '3123123', 'confidence': 0.87}}, 'skill_stack': ['askforinsurancenumber'], 'skill': 'askforinsurancenumber', 'skill_occurences': 2, 'participant_data': {}}}
     recast_response = json.loads(request.get_data())
@@ -94,7 +84,7 @@ def getInsuranceData():
     return "OK"
 
 
-@app.route('/api/v1/getIndividualDetails', methods=['POST'])
+@app.route('/api/v1/get_individual_details', methods=['POST'])
 def get_policy_individual_details():
     recast_response = json.loads(request.get_data())
     entities = list(recast_response['nlp']['entities'].keys())
@@ -124,5 +114,30 @@ def get_policy_individual_details():
     return "OK"
 
 
+@app.route('/api/v1/buy_insurance', methods=['POST'])
+def buy_assistance():
+    button_types = {
+    "type": "buttons",
+    "content": {
+      "title": "Insurance_Type",
+      "buttons": [
+        {
+          "title": "travel",
+          "type": "button",
+          "value": "travel"
+        },
+          {
+              "title": "vehicle",
+              "type": "button",
+              "value": "vehicle"
+          },
+          {
+              "title": "health",
+              "type": "button",
+              "value": "health"
+          }
+      ]
+    }
+  }
 if __name__ == '__main__':
     app.run()
